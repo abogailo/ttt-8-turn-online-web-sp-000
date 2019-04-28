a3c6a79b2a3762c886ac5d3a6f5e5e5411d3963a
Abagail Hudson <abagailanne@gmail.com>
2019-04-28 20:22:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,12 +3,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.chomp
   input_to_index(input)
-  if  valid_move?(board, input)
-     puts"cool"
-  else
-    puts "Please enter 1-9:"
-    input = gets.chomp
-  end
+  
 end
 
 
